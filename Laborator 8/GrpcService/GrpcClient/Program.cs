05eed876bff4ad66da1ac1932f0d50dc9b6a8130
cristian98st <44675214+cristian98st@users.noreply.github.com>
2020-04-28 11:00:33 +0300

    Laborator 8

---
+++ Laborator 8/GrpcService/GrpcClient/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using GrpcService;
+using Grpc.Net.Client;
+using Grpc.Core.Interceptors;
+using Google.Protobuf.WellKnownTypes;
+
+namespace GrpcGreeterClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            // The port number(5001) must match the port of the gRPC server.
+            using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new Greeter.GreeterClient(channel);
+            var postClient = new RemotePost.RemotePostClient(channel);
+            //var reply = await client.SayHelloAsync(
+            //new HelloRequest { Name = "GreeterClient" });
+            var reply2 = await postClient.AddPostAsync(
+                new PostModel
+                {
+                    Id = 1,
+                    Description = "Nimic",
+                    Domain = "Altul",
+                    Date = Timestamp.FromDateTime(DateTime.UtcNow)
+                });
+            Console.WriteLine("Result: " + reply2.Message);
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Laborator 8/GrpcService/GrpcService/Services/PostService.cs
@@ -0,0 +1,94 @@
+﻿using GrpcService;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PostComment.APIStatic;
+using Grpc.Core;
+using PostComment;
+using Google.Protobuf.WellKnownTypes;
+
+namespace GrpcService
+{
+    public class PostService : RemotePost.RemotePostBase
+    {
+        private readonly ILogger<PostService> _logger;
+
+        public PostService(ILogger<PostService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<PostModel> GetPostInfo(PostLookUpModel request, ServerCallContext context)
+        {
+            Post post = API.GetPostById(request.Id);
+
+            return Task.FromResult(new PostModel
+            {
+                Id = post.PostId,
+                Description = post.Description,
+                Domain = post.Domain,
+                Date = Timestamp.FromDateTime(post.Date)
+            });
+        }
+
+        public override Task<PostActionReply> AddPost(PostModel request, ServerCallContext context)
+        {
+            Post post = new Post();
+            post.Description = request.Description;
+            post.Domain = request.Domain;
+            post.Date = request.Date.ToDateTime();
+
+            var success = API.AddPost(post);
+
+            if (success)
+                return Task.FromResult(new PostActionReply
+                {
+                    Message = "Success"
+                });
+            else
+                return Task.FromResult(new PostActionReply
+                {
+                    Message = "Fail"
+                });
+        }
+
+        public override Task<PostActionReply> UpdatePost(PostModel request, ServerCallContext context)
+        {
+            Post post = new Post();
+            post.Description = request.Description;
+            post.Domain = request.Domain;
+            post.Date = request.Date.ToDateTime();
+
+            var success = API.UpdatePost(post);
+
+            if (success != null)
+                return Task.FromResult(new PostActionReply
+                {
+                    Message = "Success"
+                });
+            else
+                return Task.FromResult(new PostActionReply
+                {
+                    Message = "Fail"
+                });
+        }
+
+        public override Task<PostActionReply> DeletePost(PostLookUpModel request, ServerCallContext context)
+        {
+            var success = API.DeletePost(request.Id);
+
+            if (success != 0)
+                return Task.FromResult(new PostActionReply
+                {
+                    Message = "Success"
+                });
+            else
+                return Task.FromResult(new PostActionReply
+                {
+                    Message = "Fail"
+                });
+        }
+    }
+}
